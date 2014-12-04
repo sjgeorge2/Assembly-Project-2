af21c0c5a9deb1b1c58a8cea2395eef6d5d73573
sjgeorge2 <sjgeorge34@yahoo.com>
2014-12-03 20:43:55 -0900

    Create heliController.h

---
+++ src/heliController.h
@@ -0,0 +1,37 @@
+#ifndef HELICONTROLLER_H
+#define HELICONTROLLER_H
+
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
+// This is the Helicoptor class that handles movement and draws for the helicopter
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+
+class heliController
+{
+private:
+	Rectf _Helicopter;
+	ci::Vec2f _position;
+	bool _isFalling;
+	int _velocity;
+
+public:
+	heliController();
+	void updatePosition();
+	void draw();
+	void changeDirection();
+	int velocity( heliController & other);
+	int getRightX();
+	int getLeftX();
+	int getTopY();
+	int getBottomY();
+
+};
+
+
+#endif
